ba622e81b93c2029928a3b6cede02f9eb98dd815
mohamed-abdelahi-haibelty <96340736+mohamed-abdelahi-haibelty@users.noreply.github.com>
2022-07-09 12:33:21 +0000

    add js and html file

---
+++ index.js
@@ -0,0 +1,49 @@
+let previousTime,getCentiSecondInterval,stoped;
+let is_stoped = false,counter = 0,centiSecond;
+const text = document.getElementById("timer-text");
+const stopBtn = document.getElementById("stop");
+const resetBtn = document.getElementById("reset");
+const startBtn = document.getElementById("start");
+
+startBtn.addEventListener("click",function starter(){
+    previousTime = new Date().getTime();
+    getCentiSecondInterval = setInterval(getCentiSecond,10);
+});
+function getSecond(){
+    is_stoped=false;
+    previousTime = new Date().getTime();
+    counter++;
+}
+function getCentiSecond(){
+     let current = new Date().getTime();
+     centiSecond = Math.round((current-previousTime)/10);
+     if(is_stoped && centiSecond + stoped > 100 || is_stoped === false && centiSecond > 100){
+        getSecond();
+     }
+    else{
+            if(counter>=10){
+                if(is_stoped){
+                    text.innerHTML = `${counter} : ${centiSecond + stoped}`;
+                }
+                else
+                 text.innerHTML = counter + ": " + centiSecond;
+            }
+            else{
+                if(is_stoped){
+                    text.innerHTML = `0${counter} : ${centiSecond + stoped}`;
+                }
+                else
+                  text.innerHTML = "0"+ counter + ": " + centiSecond;
+            }
+        }
+}
+stopBtn.addEventListener("click",()=>{
+    clearInterval(getCentiSecondInterval);
+    is_stoped = true;
+    stoped = centiSecond;
+});
+resetBtn.addEventListener("click",()=>{
+    clearInterval(getCentiSecondInterval);
+    text.innerHTML="00:00";
+    counter = 0;
+});
